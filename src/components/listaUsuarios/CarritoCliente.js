bdf0a9ca087ec1f91f9918f8377d6dc9467612d6
Karen Alfaro <ksalfarom@gmail.com>
2022-10-27 14:09:45 +0200

    posible interfaz carrito y listaProduct

--- src/components/listaUsuarios/CarritoCliente.js
+++ src/components/listaUsuarios/CarritoCliente.js
@@ -36,7 +36,102 @@ const  Carrito = () =>
       return (
         <>
           <div className='row d-flex justify-content-around'>
+              <div className='col-12 col-lg-5 mt-5'>
+                <h3>título compras online</h3>
+                <p id="product_id">Producto </p>
+
+                <div className='rating-outer'>
+                  <div className="rating-inner" >
+
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p id="precio_producto">Valor</p>
+                <div className="stockCounter d-inline">
+                  <p id="precio_producto">Valor</p>
+                  <input type="number" className="form-control count d-inline" value="{quantity}" readOnly/>
+                </div>
             
+                <button type="button" id="carrito_btn" className="btn btn-outline-success d-inline ml-4" >
+                  Comprar
+                </button>
+                
+                <h4 className="mt-2">Descripción:</h4>
+                <p>Descripción</p>
+                
+                <h5 className="mt-2">Stock:</h5>
+                  <span className="btn btn-outline-warning minus" onClick="{decreaseQty}">-</span>
+                  <span className="btn btn-outline-primary plus" onClick="{increaseQty}">+</span>
+                
+                <p id="vendedor">Vendido por: <strong></strong></p>
+              </div>
+          </div>
+
+          <div className='row d-flex justify-content-around'>
+              <div className='col-12 col-lg-5 mt-5'>
+                <h3>título compras online</h3>
+                <p id="product_id">Producto </p>
+
+                <div className='rating-outer'>
+                  <div className="rating-inner" >
+
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p id="precio_producto">Valor</p>
+                <div className="stockCounter d-inline">
+                  <p id="precio_producto">Valor</p>
+                  <input type="number" className="form-control count d-inline" value="{quantity}" readOnly/>
+                </div>
+            
+                <button type="button" id="carrito_btn" className="btn btn-outline-success d-inline ml-4" >
+                  Comprar
+                </button>
+                
+                <h4 className="mt-2">Descripción:</h4>
+                <p>Descripción</p>
+                
+                <h5 className="mt-2">Stock:</h5>
+                <span className="btn btn-outline-warning minus" onClick="{decreaseQty}">-</span>
+                  <span className="btn btn-outline-primary plus" onClick="{increaseQty}">+</span>
+                
+                <p id="vendedor">Vendido por: <strong></strong></p>
+              </div>
+          </div>
+
+          <div className='row d-flex justify-content-around'>
+              <div className='col-12 col-lg-5 mt-5'>
+                <h3>título compras online</h3>
+                <p id="product_id">Producto </p>
+
+                <div className='rating-outer'>
+                  <div className="rating-inner" >
+
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p id="precio_producto">Valor</p>
+                <div className="stockCounter d-inline">
+                  <p id="precio_producto">Valor</p>
+                  <input type="number" className="form-control count d-inline" value="{quantity}" readOnly/>
+                </div>
+            
+                <button type="button" id="carrito_btn" className="btn btn-outline-success d-inline ml-4" >
+                  Comprar
+                </button>
+                
+                <h4 className="mt-2">Descripción:</h4>
+                <p>Descripción</p>
+                
+                <h5 className="mt-2">Stock:</h5>
+                  <span className="btn btn-outline-warning minus" onClick="{decreaseQty}">-</span>
+                  <span className="btn btn-outline-primary plus" onClick="{increaseQty}">+</span>
+                
+                <p id="vendedor">Vendido por: <strong></strong></p>
+              </div>
           </div>
         </> 
       )
